6d09d83235d71f92f9a2d5b75c67bb7e78ba0654
Max KOT <210555462@qq.com>
2022-07-21 12:06:56 +0800

    通过https://peter.website/meow-hash-cryptanalysis#differential-cryptanalysis理解了MeowHash的过程。

--- Meow-Hash-Invertibility/main.cpp
+++ Meow-Hash-Invertibility/main.cpp
@@ -2,15 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
+//#include <bitset>
 
 #include "meow_hash_x64_aesni.h"
 //#include "meow_intrinsics.h"
 //#include "meow_hash.h"
 
+using namespace std;
+
 //#define Hashed_msg sdu_cst_20220610
-//#define Msg ge_xiaoyao_202000460091
-const char* Msg = "ge_xiaoyao_202000460091";
+//#define MSG ge_xiaoyao_202000460091
+const char* MSG = "ge_xiaoyao_202000460091";
 const char* Hashed_msg = "sdu_cst_20220610";
+
+#define movdqu(A, B)  A = _mm_loadu_si128((__m128i *)(B))
 
 
 static void PrintHash(meow_u128 Hash) {
@@ -23,19 +28,26 @@ static void PrintHash(meow_u128 Hash) {
 
 int main() {
 	
-	// Make a buffer with repeating numbers (after Msg).
-	int MsgLen = strlen(Msg);
-	int Size = MsgLen / 32;
-	if (MsgLen % 32 != 0) Size++; // Add one for the last block.
-	char *Buffer = new char[Size];
-	for (int i = 0; i < MsgLen; i++) {
-		Buffer[i] = Msg[i]; // Copy Msg into Buffer.
-	}
-	for (int i = MsgLen; i < Size; i++) {
-		Buffer[i] = (char)0; // Fill the rest with zeros.
-	}
+	int MsgLen = strlen(MSG); // 23
+	int BlockNum = (MsgLen + 15) / 16; // 2
+
+	cout << "MsgLen: " << MsgLen << endl;
+	cout << "BlockNum: " << BlockNum << endl;
 	
-	
+	meow_u128 xmm0, xmm1, xmm2, xmm3, xmm4, xmm5, xmm6, xmm7; // NOTE(casey): xmm0-xmm7 are the hash accumulation lanes
+	meow_u128 xmm8, xmm9, xmm10, xmm11, xmm12, xmm13, xmm14, xmm15; // NOTE(casey): xmm8-xmm15 hold values to be appended (residual, length)
+
+	meow_u8* rcx = (meow_u8*)Hashed_msg;
+	movdqu(xmm0, Hashed_msg + 0x00); // Load the hashed message into xmm0
+	movdqu(xmm1, 0); // Load the 0's into xmm1
+	movdqu(xmm2, 0); // Load the 0's into xmm2
+	movdqu(xmm3, 0); // Load the 0's into xmm3
+	movdqu(xmm4, 0); // Load the 0's into xmm4
+	movdqu(xmm5, 0); // Load the 0's into xmm5
+	movdqu(xmm6, 0); // Load the 0's into xmm6
+	movdqu(xmm7, 0); // Load the 0's into xmm7
+
+	PrintHash(xmm0);
 	
     return 0;
 }
