69e80bf995fac794468934be3417accba8cc936b
Max Kot <73374769+MaxIkaros@users.noreply.github.com>
2022-07-24 23:56:52 -0400

    Meow hash symmetry (#17)
    * MeowHashSymmetry
    
    * 添加meow_hash_x64_aesni.h
    
    * 建立基本框架。

--- Meow-Hash-Symmetry/main.cpp
+++ Meow-Hash-Symmetry/main.cpp
@@ -10,3 +10,40 @@
 #include "meow_hash_x64_aesni.h"
 
 using namespace std;
+
+static void PrintHash(meow_u128 Hash) {
+    printf("    %08X-%08X-%08X-%08X\n",
+        MeowU32From(Hash, 3),
+        MeowU32From(Hash, 2),
+        MeowU32From(Hash, 1),
+        MeowU32From(Hash, 0));
+}
+
+
+int main() {
+    // NOTE(casey): Make a buffer with repeating numbers.
+    int Size = 16000;
+    char* Buffer = (char*)malloc(Size);
+    for (int Index = 0;
+        Index < Size;
+        ++Index)
+    {
+        Buffer[Index] = (char)Index;
+    }
+
+    // NOTE(casey): Ask Meow for the hash
+    meow_u128 Hash = MeowHash(MeowDefaultSeed, Size, Buffer);
+
+    // NOTE(casey): Extract example smaller hash sizes you might want:
+    long long unsigned Hash64 = MeowU64From(Hash, 0);
+    int unsigned Hash32 = MeowU32From(Hash, 0);
+
+    // NOTE(casey): Print the hash
+    printf("  Hash of a test buffer:\n");
+    PrintHash(Hash);
+
+    free(Buffer);
+
+	
+	return 0;
+}
