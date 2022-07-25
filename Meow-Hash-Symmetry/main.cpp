3fb0952e61f65f5ea7839460c4b36c8e547df92f
Max Kot <73374769+MaxIkaros@users.noreply.github.com>
2022-07-24 23:51:04 -0400

    Meow hash symmetry (#16)
    * MeowHashSymmetry
    
    * 添加meow_hash_x64_aesni.h

---
+++ Meow-Hash-Symmetry/main.cpp
@@ -0,0 +1,12 @@
+// https://github.com/cmuratori/meow_hash
+// https://peter.website/meow-hash-cryptanalysis
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+//#include <bitset>
+
+#include "meow_hash_x64_aesni.h"
+
+using namespace std;
